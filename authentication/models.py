b6d1753d49e0d0f9c360c824786fde6e8a2e48ca
Trilok Chaudhary <trilokchy999@gmail.com>
2024-03-03 20:51:33 +0545

    sixth commit

--- authentication/models.py
+++ authentication/models.py
@@ -10,19 +10,30 @@ genre_choices = (
     ("Jazz","Jazz")
 )
 
+day_choices = (
+    ("Sunday", "Sunday"),
+    ("Monday","Monday"),
+    ("Tuesday","Tuesday"),
+    ("Wednesday","Wednesday"),
+    ("Thursday","Thursday"),
+    ("Friday","Friday"),
+    ("Saturday","Saturday")
+)
+
 class Restaurant(models.Model):
     rest_name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     genre = models.CharField(max_length=100, choices=genre_choices)
     performer = models.CharField(max_length=100, default="")
     ratings = models.CharField(max_length=100, default="")
-    day = models.CharField(max_length=100, default="")
-    time = models.CharField(max_length=100, default="")
-    rest_image = models.ImageField(upload_to='restaurant', default="")
+    # day = models.CharField(max_length=100, default="")
+    # time = models.CharField(max_length=100, default="")
 
+    day = models.CharField(max_length=100, choices=day_choices)
     
-    # start_time = models.TimeField(null=True,blank=True)
-    # end_time = models.TimeField(null=True,blank=True)
+    start_time = models.TimeField(null=True,blank=True)
+    end_time = models.TimeField(null=True,blank=True)
 
+    rest_image = models.ImageField(upload_to='restaurant', default="")
     def __str__(self):
         return self.rest_name
